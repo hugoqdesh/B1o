59ec060262e2fb7909d7426340209fadc693f58d
HJ <gitjohan5@gmail.com>
2025-03-14 15:01:36 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import Header from "@/components/landing-page/header";
 import Waitlist from "@/components/landing-page/waitlist";
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
-import Image from "next/image";
 
 export default function Home() {
 	return (
